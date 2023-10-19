b9b586882bd6b3742e1b398328cf2ea3b35040a9
Pruone <tomandavid1@seznam.cz>
2023-10-19 19:23:01 +0200

    hadani_cisel_final

--- guessing_numbers.py
+++ guessing_numbers.py
@@ -11,3 +11,18 @@ while True:
         print("Neplatný vstup!")
 
 nahodne_cislo = random.randint(1,horni_hranice)
+pokusy = 0
+while True:
+    try:
+        uzivatelsky_vstup = int(input("Uhádněte číslo od 1 do " + str(horni_hranice) + ": ").strip())
+    except:
+        print("Neplatný vstup!")
+        continue
+    pokusy += 1
+    if uzivatelsky_vstup == nahodne_cislo:
+        print("Správná odpověď!\nCelkový počet pokusů:", pokusy)
+        break
+    elif uzivatelsky_vstup > nahodne_cislo:
+        print("Zkuste menší číslo")
+    else:
+        print("Zkuste větší číslo")
